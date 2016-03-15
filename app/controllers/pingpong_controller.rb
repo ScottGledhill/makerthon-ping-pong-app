fae7ec8db476fc201afa3483baba33542f26a5af
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-15 14:17:10 +0000

    controller initialized

---
+++ app/controllers/pingpong_controller.rb
@@ -0,0 +1,15 @@
+class PingpongController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+  
+end
---
+++ app/helpers/pingpong_helper.rb
@@ -0,0 +1,2 @@
+module PingpongHelper
+end
