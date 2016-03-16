49976199cb857612cc2fc05f9683ac812922c8f2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-16 11:55:28 +0000

    games controller updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
 
   def index
-    
+    @game = Game.new
   end
 
   def new
@@ -13,7 +13,12 @@ class GamesController < ApplicationController
   def show
   end
 
-  def edit
+  def update
+    if @game.update(@game_params)
+      render json: @game
+    else
+      render json: @game
+    end
   end
 
   def destroy
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,4 +9,5 @@ class PlayersController < ApplicationController
 
   end
 
+
 end
