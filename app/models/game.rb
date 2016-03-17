8886ebad1f6be32fc2bf773f8bef48c4d58d5296
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-17 10:27:45 +0000

    day 3 begin

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ActiveRecord::Base
   has_many :players
+  # after_save :update_score
+  #
+  # def update_record
+  #   self.player.update_score!
+  # end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,9 @@
 class Player < ActiveRecord::Base
   has_many :games
   validates :name, length: {minimum: 3}
+
+  # def update_score!
+  #   self.update_column(:wins, self.game += 1)
+  #   self.update_column(:losses, self.game += 1)
+  # end
 end
