eda335aaa98beae7e01fff2bf8923e3ce2897a4b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-18 14:28:39 +0000

    presentation ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
 player_list = [
-  [ "Zeshan", 0,0,0 ],
-  [ "Hannah", 0,0,0 ],
-  [ "Scott", 0,0,0 ],
+  [ "Zeshan", 14,14,50 ],
+  [ "Hannah", 7,7,50 ],
+  [ "Scott", 98,0,100 ],
   [ "Mo", 0,0,0 ],
   [ "Arnold", 0,0,0 ],
   [ "Rufus", 0,0,0 ],
-  [ "Rachel", 0,0,0 ],
-  [ "Sean", 0,0,0 ],
+  [ "Rachel", 0,7,0 ],
+  [ "Sean", 2,2,50 ],
   [ "Constantin", 0,0,0 ],
-  [ "Marcus", 0,0,0 ],
+  [ "Marcus", 0,50,0 ],
+  ["Nikesh", 0,100,0]
 ]
 
 player_list.each do |name, wins, losses, win_percentage|
