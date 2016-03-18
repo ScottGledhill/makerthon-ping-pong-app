93824e758df9a8e62990cc4f0b521afeda5e0d7e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-03-18 14:20:14 +0000

    finalised before merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+player_list = [
+  [ "Zeshan", 0,0,0 ],
+  [ "Hannah", 0,0,0 ],
+  [ "Scott", 0,0,0 ],
+  [ "Mo", 0,0,0 ],
+  [ "Arnold", 0,0,0 ],
+  [ "Rufus", 0,0,0 ],
+  [ "Rachel", 0,0,0 ],
+  [ "Sean", 0,0,0 ],
+  [ "Constantin", 0,0,0 ],
+  [ "Marcus", 0,0,0 ],
+]
+
+player_list.each do |name, wins, losses, win_percentage|
+  Player.create( name: name, wins: wins, losses: losses, win_percentage: win_percentage)
+end
